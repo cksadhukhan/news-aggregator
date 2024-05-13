177954963ba79b4f4b0c00683aa4bbe85126c5ed
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2024-05-13 23:18:31 +0530

    app update

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const { userRoute, newsRoute } = require("./routes");
 const { loggerMiddleware } = require("./middlewares");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(loggerMiddleware);
 
