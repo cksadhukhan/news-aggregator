146bba56f3e982647d3b78179b5a30bd6096f6c2
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2024-05-13 23:16:08 +0530

    auth middleware added

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    jwt.verify(authHeader, "secret", (err, user) => {
+      if (err) {
+        return res.status(403).json({ error: "Invalid token" });
+      }
+      req.user = user;
+      next();
+    });
+  } catch {
+    res.status(500).json({ error: "Error occured while verifying the token" });
+    next();
+  }
+};
+
+module.exports = verifyToken;
